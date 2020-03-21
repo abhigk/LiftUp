99c2be681ddefb115313954f5d13de18f5748a67
Abhi <abhishekgupta0595@gmail.com>
2020-03-21 18:56:05 +1100

    added navbar and dummy components

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CreateExercise extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are on the Create Exercise component!</p>
+      </div>
+    );
+  }
+}
