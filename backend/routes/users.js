26dadb712c9bf4992f432e6f09d0045553fd08b0
Abhi <abhishekgupta0595@gmail.com>
2020-03-23 20:30:29 +1100

    completed

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,6 +8,7 @@ router.route("/").get((req, res) => {
 });
 
 router.route("/add").post((req, res) => {
+  console.log(req.body);
   const username = req.body.username;
 
   const newUser = new User({ username });
